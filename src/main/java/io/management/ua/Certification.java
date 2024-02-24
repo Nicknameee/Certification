e722dbc5bc5989cb4ac461eff7bee737eae49e8f
Nicknameee <igrunka1991@gmail.com>
2024-02-24 21:34:42 +0200

    Eureka

--- src/main/java/io/management/ua/Certification.java
+++ src/main/java/io/management/ua/Certification.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = "io.management")
 @Slf4j
 @RequiredArgsConstructor
+@EnableEurekaClient
 public class Certification {
     public static void main(String[] args) {
         SpringApplication.run(Certification.class);
